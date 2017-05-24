d4f1e351aed8b124318206bdb9f4a83c96efd0a8
saleemjaffer <saleem@coverfoxmail.com>
2017-05-24 11:38:35 +0530

    conf file generation mods

--- config.go
+++ config.go
@@ -1,145 +1,182 @@
 package amalgam
 
 import (
-    "log"
-    "os"
-    "os/user"
-    "path/filepath"
-    "fmt"
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
 
-    _ "github.com/lib/pq"
-    "github.com/namsral/flag"
+	_ "github.com/lib/pq"
+	"github.com/namsral/flag"
 )
 
 var (
-    Listen = ":8000"
-    DbName = ""
-    DbHost = "127.0.0.1"
-    DbPort = 5432
-    DbUser = "user"
-    DbPass = ""
-    Verbosity = 4
-    Secret = ""
-    Config = ""
+	Listen     = ":8000"
+	DbName     = ""
+	DbHost     = "127.0.0.1"
+	DbPort     = 5432
+	DbUser     = "user"
+	DbPass     = ""
+	Verbosity  = 4
+	Secret     = ""
+	Config     = ""
+	CreateConf = false
 
-    FLAGSET *flag.FlagSet = nil
+	FLAGSET *flag.FlagSet = nil
 
-    Confs map[string]interface{}
+	Confs map[string]interface{}
 )
 
-func CreateFlag(
-varName interface{},
-name string,
-defVal interface{},
-description string,
+func StringFlag(
+	varName *string,
+	name string,
+	defVal string,
+	description string,
 ) {
-    var intVal int
-    var stringVal string
-    var ok = false
-
-    stringVal, ok = defVal.(string)
-    if !ok {
-        intVal, ok = defVal.(int)
-        if !ok {
-            panic("Unhandled flag type!")
-        }
-
-        Confs[name] = defVal
-
-        v, ok := varName.(*int)
-        if !ok {
-            panic("wrong pointer type")
-        }
-        FLAGSET.IntVar(v, name, intVal, description)
-
-    } else {
-        Confs[name] = defVal
-
-        v, ok := varName.(*string)
-        if !ok {
-            panic("wrong pointer type")
-        }
-        FLAGSET.StringVar(v, name, stringVal, description)
-    }
+	Confs[name] = defVal
+	FLAGSET.StringVar(varName, name, defVal, description)
 }
 
+func IntFlag(
+	varName *int,
+	name string,
+	defVal int,
+	description string,
+) {
+	Confs[name] = defVal
+	FLAGSET.IntVar(varName, name, defVal, description)
+}
+
+func BoolFlag(
+	varName *bool,
+	name string,
+	defVal bool,
+	description string,
+) {
+	Confs[name] = defVal
+	FLAGSET.BoolVar(varName, name, defVal, description)
+}
+
+//func CreateFlag(
+//	varName interface{},
+//	name string,
+//	defVal interface{},
+//	description string,
+//) {
+//	var intVal int
+//	var stringVal string
+//	var ok = false
+//
+//	stringVal, ok = defVal.(string)
+//	if !ok {
+//		intVal, ok = defVal.(int)
+//		if !ok {
+//			panic("Unhandled flag type!")
+//		}
+//
+//		Confs[name] = defVal
+//
+//		v, ok := varName.(*int)
+//		if !ok {
+//			panic("wrong pointer type")
+//		}
+//		FLAGSET.IntVar(v, name, intVal, description)
+//
+//	} else {
+//		Confs[name] = defVal
+//
+//		v, ok := varName.(*string)
+//		if !ok {
+//			panic("wrong pointer type")
+//		}
+//		FLAGSET.StringVar(v, name, stringVal, description)
+//	}
+//}
+
 func init() {
-    filename, _ := os.Executable()
-    Config = filepath.Base(filename) + ".conf"
+	filename, _ := os.Executable()
+	Config = filepath.Base(filename) + ".conf"
 
-    Confs = make(map[string]interface{})
+	Confs = make(map[string]interface{})
 
-    user, err := user.Current()
-    if err == nil {
-        DbUser = user.Username
-    }
+	user, err := user.Current()
+	if err == nil {
+		DbUser = user.Username
+	}
 
-    f := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-    FLAGSET = f
+	f := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	FLAGSET = f
 
-    CreateFlag(&Listen, "listen", Listen, "http address")
-    CreateFlag(
-        &Verbosity, "verbosity", Verbosity,
-        "logging verbosity, 0: none, 4: all",
-    )
-    CreateFlag(&Config, "config", Config, "config file")
-    CreateFlag(&DbName, "dbname", DbName, "database name")
-    CreateFlag(&DbHost, "dbhost", DbHost, "database host")
-    CreateFlag(&DbPort, "dbport", DbPort, "database port")
-    CreateFlag(&DbUser, "dbuser", DbUser, "database user")
-    CreateFlag(&DbPass, "dbpass", DbPass, "database password")
-    CreateFlag(&Secret, "secret", Secret, "django secret key")
+	StringFlag(&Listen, "listen", Listen, "http address")
+	IntFlag(
+		&Verbosity, "verbosity", Verbosity,
+		"logging verbosity, 0: none, 4: all",
+	)
+	StringFlag(&DbName, "dbname", DbName, "database name")
+	StringFlag(&DbHost, "dbhost", DbHost, "database host")
+	IntFlag(&DbPort, "dbport", DbPort, "database port")
+	StringFlag(&DbUser, "dbuser", DbUser, "database user")
+	StringFlag(&DbPass, "dbpass", DbPass, "database password")
+	StringFlag(&Secret, "secret", Secret, "django secret key")
+	BoolFlag(&CreateConf, "create-conf", CreateConf, "")
 }
 
 func Init() {
-    if 2 == len(os.Args) && os.Args[1] == "confs" {
-        n, err := filepath.Abs(filepath.Dir(os.Args[0]))
-        if err != nil {
-            log.Fatal(err)
-        }
+	if err := FLAGSET.Parse(os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
 
-        prodDir := filepath.Dir(n)
+	if CreateConf {
+		n, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        exName, _ := os.Executable()
+		prodDir := filepath.Dir(n)
 
-        confFile := filepath.Join(prodDir, filepath.Base(exName) + ".conf")
+		exName, _ := os.Executable()
 
-        if _, err := os.Stat(confFile); err == nil {
-            panic("conf files already present!")
-        } else {
-            writeConfFile(confFile)
-        }
-    }
+		confFile := filepath.Join(prodDir, filepath.Base(exName)+".conf")
 
-    if err := FLAGSET.Parse(os.Args[1:]); err != nil {
-        log.Fatal(err)
-    }
+		if _, err := os.Stat(confFile); err == nil {
+			panic("conf files already present!")
+		} else {
+			writeConfFile(confFile)
+		}
+	}
 
-    log.Println("config_parsed", "args", os.Args[1:], "flags", FLAGSET.Args())
+	StringFlag(&Config, "config", Config, "config file")
+
+	if err := FLAGSET.Parse(os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("config_parsed", "args", os.Args[1:], "flags", FLAGSET.Args())
 
 }
 
-func writeConfFile(confFile string ) {
-    f, err := os.Create(confFile)
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
+func writeConfFile(confFile string) {
+	f, err := os.Create(confFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-    for key, val := range(Confs) {
-        var stringVal string
-        var intVal int
-        var ok bool
+	for key, val := range Confs {
+		var stringVal string
+		var intVal int
+		var ok bool
 
-        stringVal, ok = val.(string)
-        if ok {
-            f.WriteString(fmt.Sprintf("%s %s\n", key, stringVal))
-        }
+		stringVal, ok = val.(string)
+		if ok {
+			f.WriteString(fmt.Sprintf("%s %s\n", key, stringVal))
+		}
 
-        intVal, ok = val.(int)
-        if ok {
-            f.WriteString(fmt.Sprintf("%s %d\n", key, intVal))
-        }
-    }
+		intVal, ok = val.(int)
+		if ok {
+			f.WriteString(fmt.Sprintf("%s %d\n", key, intVal))
+		}
+	}
 
 }
